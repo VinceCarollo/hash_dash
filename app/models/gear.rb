96d870a5ab4a3583244e121fc1a96ab43afd6db2
Vince Carollo <vincecarollo@gmail.com>
2021-08-21 22:34:15 -0500

    Add papers to gear categor

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,6 +1,6 @@
 class Gear < Product
   after_initialize :init
-  enum category: [:lighter, :pipe, :bowl, :nail, :torch, :bong, :apparel]
+  enum category: [:lighter, :pipe, :bowl, :papers, :nail, :torch, :bong, :apparel]
 
   def init
     self.contains_thc = false
