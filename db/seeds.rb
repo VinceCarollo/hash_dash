4ba6d48e2988c3489ebec84a321663633b0ccd43
Vince Carollo <vincecarollo@gmail.com>
2021-09-24 12:26:36 -0500

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flower.create(
+  name: 'Flower',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
+Concentrate.create(
+  name: 'Concentrate',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
+Edible.create(
+  name: 'Edible',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
+Gear.create(
+  name: 'Gear',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
+Topical.create(
+  name: 'Topical',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
+VapePen.create(
+  name: 'Vape Pen',
+  brand: Faker::Cannabis.brand,
+  description: Faker::Cannabis.health_benefit
+)
