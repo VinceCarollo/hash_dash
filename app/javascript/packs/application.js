b496121d1cbc34457351bf25c5a2cf6414361735
Vince Carollo <vincecarollo@gmail.com>
2021-09-25 22:20:18 -0500

    Remove turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -15,5 +14,3 @@ ActiveStorage.start()
 import "bootstrap"
 
 import "controllers"
-
-import "@hotwired/turbo-rails"
