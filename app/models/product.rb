f6eb402aad0d750301d04a85a844938d22e76202
Vince Carollo <vincecarollo@gmail.com>
2021-09-25 19:31:05 -0500

    Block saving of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ApplicationRecord
   validate :type, :name, :category
 
   enum species_category: [:indica, :sativa, :hybrid]
+
+  # Creation of product disallowed
+  validate :block_save
+
+  private
+
+  def block_save
+    errors.add(:id, ': A Product must have a type')
+  end
 end
